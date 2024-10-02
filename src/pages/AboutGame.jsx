f65b324ca073c89e02523c38f4f948970cfd139e
AmmarMubashir <muhammadammarmubashir@gmail.com>
2024-10-02 21:08:43 +0500

    about game and setting info video added

--- src/pages/AboutGame.jsx
+++ src/pages/AboutGame.jsx
@@ -1,74 +1,95 @@
 import React from "react";
 import RightNav from "./components/RightNav";
-import { useNavigate } from "react-router-dom";
+import AboutGameVideo from "../assets/AboutGame.mp4";
 
 const AboutGame = () => {
-  const navigate = useNavigate();
   return (
+    // <div className="w-full h-[100vh] flex bg-[#fbb748] relative overflow-hidden">
+    //   <RightNav />
+    //   <div className=" h-[100vh] flex-1 flex flex-col py-4  items-center overflow-auto">
+    //     <h1 className="mb-7 mt-5 text-[1.4rem] sm:text-[1.8rem] text-[#1b375f] font-bold font-mono">
+    //       About the game
+    //     </h1>
+    //     <div className="w-[90%] lg:w-[80%] mx-auto text-[1.1rem] lg:text-[1.4rem]  bg-white rounded py-4 px-3 flex flex-col gap-4">
+    //       <p>
+    //         This is a business simulation game where participants learn
+    //         practical skills for running a business. Up to five teams can
+    //         compete against each other. Each team consists of three to five
+    //         members who take on specific roles within the business, such as
+    //         marketing, finance, operations etc.
+    //       </p>
+    //       <p>
+    //         The game starts just before Quarter 1 (as some events have already
+    //         happened) and the teams start at the same point. The purpose of the
+    //         game is to learn about various aspects of running a business and
+    //         dealing with real-life issues. The fun part is that they are
+    //         competing against other teams, and the team with the most profit
+    //         wins.
+    //       </p>
+    //       <p>
+    //         Throughout the game, different opportunities and events arise that
+    //         will affect their business depending on the choices they make. There
+    //         are also activities that either as a team or each member needs to
+    //         carry out, in their role, and bring back to the team to help drive
+    //         the business forward by earning more income or incurring more
+    //         expenditure.
+    //       </p>
+    //       <p>
+    //         At the end of each quarter the teams will discover how they are
+    //         performing and what their competitors are up to. This information
+    //         will help them make those tough decisions, try to correct previous
+    //         errors and keep your customers happy and loyal whilst attracting new
+    //         ones. They will look at learning from their mistakes and finding
+    //         ways to mitigate them.
+    //       </p>
+    //       <p>
+    //         Whichever team makes the most profit at the end will decide which
+    //         team is the overall winner. This means balancing profit with
+    //         customer service, staff morale and successful marketing
+    //       </p>
+    //       <p>
+    //         The teams will create a basic business plan for their bakery. The
+    //         game is aimed at people who may already have a young business and
+    //         want to improve their management skills, or those who are thinking
+    //         about starting their own business. They will also understand the
+    //         importance of planning with a view to in creating their own business
+    //         plan.
+    //       </p>
+    //       <p>
+    //         By the end of the game, each participant be able to produce their
+    //         own draft business plan, ready for presenting to funders or
+    //         investors.
+    //       </p>
+    //       {/* <div>
+    //         <button
+    //           className="px-4 py-2 rounded bg-[#1B375F] text-white w-max ml-auto block"
+    //           onClick={() => navigate("/setting-the-scene")}
+    //         >
+    //           Next
+    //         </button>
+    //       </div> */}
+    //     </div>
+    //   </div>
+    // </div>
     <div className="w-full h-[100vh] flex bg-[#fbb748] relative overflow-hidden">
       <RightNav />
-      <div className=" h-[100vh] flex-1 flex flex-col py-4  items-center overflow-auto">
-        <h1 className="mb-7 mt-5 text-[1.4rem] sm:text-[1.8rem] text-[#1b375f] font-bold font-mono">
+      <div className=" h-[100vh] flex-1 flex flex-col justify-center  items-center ">
+        <h1 className="mb-7 mt-5 text-[1.8rem] text-[#1b375f] font-bold font-mono">
           About the game
         </h1>
-        <div className="w-[90%] lg:w-[80%] mx-auto text-[1.1rem] lg:text-[1.4rem]  bg-white rounded py-4 px-3 flex flex-col gap-4">
-          <p>
-            This is a business simulation game where participants learn
-            practical skills for running a business. Up to five teams can
-            compete against each other. Each team consists of three to five
-            members who take on specific roles within the business, such as
-            marketing, finance, operations etc.
-          </p>
-          <p>
-            The game starts just before Quarter 1 (as some events have already
-            happened) and the teams start at the same point. The purpose of the
-            game is to learn about various aspects of running a business and
-            dealing with real-life issues. The fun part is that they are
-            competing against other teams, and the team with the most profit
-            wins.
-          </p>
-          <p>
-            Throughout the game, different opportunities and events arise that
-            will affect their business depending on the choices they make. There
-            are also activities that either as a team or each member needs to
-            carry out, in their role, and bring back to the team to help drive
-            the business forward by earning more income or incurring more
-            expenditure.
-          </p>
-          <p>
-            At the end of each quarter the teams will discover how they are
-            performing and what their competitors are up to. This information
-            will help them make those tough decisions, try to correct previous
-            errors and keep your customers happy and loyal whilst attracting new
-            ones. They will look at learning from their mistakes and finding
-            ways to mitigate them.
-          </p>
-          <p>
-            Whichever team makes the most profit at the end will decide which
-            team is the overall winner. This means balancing profit with
-            customer service, staff morale and successful marketing
-          </p>
-          <p>
-            The teams will create a basic business plan for their bakery. The
-            game is aimed at people who may already have a young business and
-            want to improve their management skills, or those who are thinking
-            about starting their own business. They will also understand the
-            importance of planning with a view to in creating their own business
-            plan.
-          </p>
-          <p>
-            By the end of the game, each participant be able to produce their
-            own draft business plan, ready for presenting to funders or
-            investors.
-          </p>
-          {/* <div>
-            <button
-              className="px-4 py-2 rounded bg-[#1B375F] text-white w-max ml-auto block"
-              onClick={() => navigate("/setting-the-scene")}
-            >
-              Next
-            </button>
-          </div> */}
+        <div className="w-[95%] md:w-[75%] lg:w-[60%] flex flex-col justify-center ">
+          <div className="relative w-full h-0 pb-[56.25%]">
+            {" "}
+            {/* 16:9 Aspect Ratio */}
+            <iframe
+              src={AboutGameVideo}
+              className="absolute top-0 left-0 w-full h-full rounded"
+              title="Responsive Video"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/SettingScene.jsx
+++ src/pages/SettingScene.jsx
@@ -1,68 +1,89 @@
 import React from "react";
 import RightNav from "./components/RightNav";
-import { useNavigate } from "react-router-dom";
+import SettingSceneVideo from "../assets/StartupInfo.mp4";
 
 const SettingScene = () => {
-  const navigate = useNavigate();
   return (
+    // <div className="w-full h-[100vh] flex bg-[#fbb748] relative overflow-hidden">
+    //   <RightNav />
+    //   <div className=" h-[100vh] py-4 flex-1 flex flex-col  items-center overflow-auto">
+    //     <h1 className="mb-7 mt-5 mx-auto text-[1.4rem] sm:text-[1.8rem] text-[#1b375f] font-bold font-mono">
+    //       Setting Scene to the game
+    //     </h1>
+    //     <div className="w-[90%] lg:w-[80%] mx-auto text-[1.1rem] lg:text-[1.4rem]  bg-white rounded py-4 px-3 flex flex-col gap-4">
+    //       <p>
+    //         My Bread n Butter is a new venture dedicated to delivering a
+    //         delightful array of freshly baked goods crafted with meticulous
+    //         attention to quality and taste. Specialising in an assortment of
+    //         breads, pastries, cakes, and cookies, this home-based bakery
+    //         emphasises the use of locally sourced ingredients to ensure the
+    //         utmost freshness and flavour in every product. Each item is made
+    //         with the care and passion of a home kitchen, bringing a personal
+    //         touch to the art of baking that mass-produced goods simply cannot
+    //         match.
+    //       </p>
+    //       <p>
+    //         The business operates primarily through online orders and local
+    //         deliveries, making it convenient for customers to enjoy homemade
+    //         baked goods without leaving the comfort of their homes. By
+    //         maintaining a robust online presence, My Bread n Butter provides an
+    //         easy and accessible platform for customers to place orders,
+    //         customize their selections, and schedule deliveries. Participation
+    //         in local markets and events further enhances community engagement
+    //         and brand visibility, allowing for direct interaction with customers
+    //         and immediate feedback.
+    //       </p>
+    //       <p>
+    //         Product offerings include a diverse selection of breads such as
+    //         white, whole grain, and sourdough, alongside a variety of pastries
+    //         like croissants and Danish delights. The cake selection ranges from
+    //         classic birthday cakes to intricate cupcakes, each designed to cater
+    //         to special occasions and personal preferences. Cookies, ranging from
+    //         classic chocolate chip to oatmeal raisin, add a touch of sweetness
+    //         to the daily offerings. The flexibility to customize orders ensures
+    //         that customer needs and preferences are always met, whether for a
+    //         family gathering or a special event.
+    //       </p>
+    //       <p>
+    //         By focusing on high-quality, homemade products and leveraging local
+    //         ingredients, My Bread n Butter stands out in the competitive bakery
+    //         industry. This approach not only supports local farmers and
+    //         suppliers but also aligns with growing consumer preferences for
+    //         artisanal and specialty baked goods. The commitment to quality,
+    //         combined with a personalised customer experience, positions the
+    //         bakery as a trusted provider of delightful baked treats, ready to
+    //         satisfy the cravings and culinary desires of the local community.
+    //       </p>
+    //       {/* <div>
+    //         <button
+    //           className="px-4 py-2 rounded bg-[#1B375F] text-white w-max ml-auto block"
+    //           onClick={() => navigate("/startup")}
+    //         >
+    //           Next
+    //         </button>
+    //       </div> */}
+    //     </div>
+    //   </div>
+    // </div>
     <div className="w-full h-[100vh] flex bg-[#fbb748] relative overflow-hidden">
       <RightNav />
-      <div className=" h-[100vh] py-4 flex-1 flex flex-col  items-center overflow-auto">
-        <h1 className="mb-7 mt-5 mx-auto text-[1.4rem] sm:text-[1.8rem] text-[#1b375f] font-bold font-mono">
-          Setting Scene to the game
+      <div className=" h-[100vh] flex-1 flex flex-col justify-center  items-center ">
+        <h1 className="mb-7 mt-5 text-[1.8rem] text-[#1b375f] font-bold font-mono">
+          Setting the scene
         </h1>
-        <div className="w-[90%] lg:w-[80%] mx-auto text-[1.1rem] lg:text-[1.4rem]  bg-white rounded py-4 px-3 flex flex-col gap-4">
-          <p>
-            My Bread n Butter is a new venture dedicated to delivering a
-            delightful array of freshly baked goods crafted with meticulous
-            attention to quality and taste. Specialising in an assortment of
-            breads, pastries, cakes, and cookies, this home-based bakery
-            emphasises the use of locally sourced ingredients to ensure the
-            utmost freshness and flavour in every product. Each item is made
-            with the care and passion of a home kitchen, bringing a personal
-            touch to the art of baking that mass-produced goods simply cannot
-            match.
-          </p>
-          <p>
-            The business operates primarily through online orders and local
-            deliveries, making it convenient for customers to enjoy homemade
-            baked goods without leaving the comfort of their homes. By
-            maintaining a robust online presence, My Bread n Butter provides an
-            easy and accessible platform for customers to place orders,
-            customize their selections, and schedule deliveries. Participation
-            in local markets and events further enhances community engagement
-            and brand visibility, allowing for direct interaction with customers
-            and immediate feedback.
-          </p>
-          <p>
-            Product offerings include a diverse selection of breads such as
-            white, whole grain, and sourdough, alongside a variety of pastries
-            like croissants and Danish delights. The cake selection ranges from
-            classic birthday cakes to intricate cupcakes, each designed to cater
-            to special occasions and personal preferences. Cookies, ranging from
-            classic chocolate chip to oatmeal raisin, add a touch of sweetness
-            to the daily offerings. The flexibility to customize orders ensures
-            that customer needs and preferences are always met, whether for a
-            family gathering or a special event.
-          </p>
-          <p>
-            By focusing on high-quality, homemade products and leveraging local
-            ingredients, My Bread n Butter stands out in the competitive bakery
-            industry. This approach not only supports local farmers and
-            suppliers but also aligns with growing consumer preferences for
-            artisanal and specialty baked goods. The commitment to quality,
-            combined with a personalised customer experience, positions the
-            bakery as a trusted provider of delightful baked treats, ready to
-            satisfy the cravings and culinary desires of the local community.
-          </p>
-          {/* <div>
-            <button
-              className="px-4 py-2 rounded bg-[#1B375F] text-white w-max ml-auto block"
-              onClick={() => navigate("/startup")}
-            >
-              Next
-            </button>
-          </div> */}
+        <div className="w-[95%] md:w-[75%] lg:w-[60%] flex flex-col justify-center ">
+          <div className="relative w-full h-0 pb-[56.25%]">
+            {" "}
+            {/* 16:9 Aspect Ratio */}
+            <iframe
+              src={SettingSceneVideo}
+              className="absolute top-0 left-0 w-full h-full rounded"
+              title="Responsive Video"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
